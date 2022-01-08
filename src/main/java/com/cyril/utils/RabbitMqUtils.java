4910a8590927fb4854ac40fed059ac149fadf60a
Cyril <1939945226@qq.com>
2022-01-08 12:52:38 +0800

    交换机fanout

--- src/main/java/com/cyril/utils/RabbitMqUtils.java
+++ src/main/java/com/cyril/utils/RabbitMqUtils.java
@@ -11,7 +11,8 @@ import com.rabbitmq.client.ConnectionFactory;
  */
 public class RabbitMqUtils {
     //得到一个连接的 channel
-    public static Channel getChannel() throws Exception {
+    public static Channel getChannel(){
+        try {
         //创建一个连接工厂
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("112.74.106.34");
@@ -20,5 +21,22 @@ public class RabbitMqUtils {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
         return channel;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    //关闭通道和关闭连接工具方法
+    public static void closeConnectionAndChannel(Channel channel, Connection conn){
+        try {
+            if(channel !=null) {
+                channel.close();
+            }
+            if(conn != null) {
+                conn.close();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
